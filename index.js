9b40b074e26fab98424ae7a8947254f6b3a6d242
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-06-27 10:44:31 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats.push("Ralph");
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat(Garfield) {
+  cats.pop(Garfield);
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+  cats.shift(Milo);
+}
+
+function destructivelyAppendCat(Broom) {
+  cats.push(Broom);
+}
+function appendCat(Broom) {
+  return [...cats, Broom];
+}
+
+function prependCat(Arnold) {
+  return [Arnold, ...cats];
+}
+
+function removeLastCat(cats) {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(cats) {
+  return cats.slice(1);
+}
+
+ //Perform operations on the cats array
+destructivelyPrependCat("Tom");
+destructivelyRemoveLastCat();
+destructivelyRemoveFirstCat();
+// Add a cat to the end of the array
+
+// Display the final state of the cats array
+console.log(cats);
